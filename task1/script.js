db9f31c281f27ec121d509161c1dcdd73b8b33c7
vmik7 <vmik7@yandex.ru>
2021-07-04 14:18:40 +0300

    Task 1

--- task1/script.js
+++ task1/script.js
@@ -1,1 +1,19 @@
-alert('Hello world!');
+const a = [7, 1, 3, 4, 11];
+console.log('input:', a);
+
+const ans = bubbleSort(a);
+console.log('output:', ans);
+
+function bubbleSort(a) {
+    const res = a;
+    for (let i = 0; i < res.length; i++) {
+        for (let j = i + 1; j < res.length; j++) {
+            if (res[j - 1] > res[j]) {
+                let tmp = res[j];
+                res[j] = res[j - 1];
+                res[j - 1] = tmp;
+            }
+        }
+    }
+    return res;
+}
